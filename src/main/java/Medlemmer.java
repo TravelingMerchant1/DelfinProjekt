77e12d0dbf1379575f6a6c8947f27c51ed274a43
Enes Kocer <enes0539@stud.kea.dk>
2022-11-22 09:29:07 +0100

    Initial commit

---
+++ src/main/java/Medlemmer.java
@@ -0,0 +1,13 @@
+public class Medlemmer {
+
+    private String navn;
+    private int alder;
+    private String køn;
+    private boolean aktivitetsForm;
+    private boolean hold;
+    private boolean konkurrenceSvømmer;
+
+
+
+
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner input = new Scanner(System.in);
+
+    public void menuText(){
+        System.out.println("Velkommen til Delfinens administrative system");
+        System.out.println("1) Indmeldelse af nye medlemmer");
+        System.out.println("2) Oversigt over medlemmer");
+        System.out.println("3) Opdater medlemmers resultater");
+        System.out.println("5) Kontigent oversigt");
+        System.out.println("6) Oversigt over hold");
+        System.out.println("7) Exit");
+    }
+
+    public void start() {
+        String menuInput = input.nextLine().toLowerCase();
+        switch (menuInput) {
+            case "1":
+
+            case "2":
+
+            case "3":
+
+            case "4":
+
+            case "5":
+
+            case "6":
+
+            case "7":
+
+        }
+
+    }
+}
